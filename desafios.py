389c78e56ed4900589aebd7499b43e12358bf5c5
Manoel Lima <manoelsslima@yahoo.com.br>
2025-11-07 00:17:05 -0400

    feat: função para listar os comandos básicos do git

--- desafios.py
+++ desafios.py
@@ -26,7 +26,7 @@ def listar_comandos_git_basicos():
     Exemplo de saída:
     ["git init", "git add", "git commit", "git status", "git push"]
     """
-    pass
+    print(["git init", "git add", "git commit", "git status", "git push"])
 
 
 def criar_mensagem_commit(funcao_nome):
